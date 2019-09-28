bc531c8a8a53f78a0662cac875590fb7c3c3e3d3
tobias <buecktobias@gmail.com>
2019-09-28 16:16:46 +0200

    working djikstra for ex 3

--- main.cpp
+++ main.cpp
@@ -1,46 +1,94 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <limits.h>
+#include <stdio.h>
+// Number of vertices in the graph
+#define V 9
 using namespace std;
+// A utility function to find the vertex with minimum distance value, from
+// the set of vertices not yet included in shortest path tree
+int minDistance(vector<int> dist, bool sptSet[])
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
 
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
 
-bool sortcol( const vector<double>& v1,
-              const vector<double>& v2 ) {
-    return v1[1] > v2[1];
+    return min_index;
 }
 
+// A utility function to print the constructed distance array
+int printSolution(int dist[])
+{
+    printf("Vertex \t\t Distance from Source\n");
+    for (int i = 0; i < V; i++)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
 
-int main() {
-    int amountOfPortions;
-    int amountOfFood;
-    cin >> amountOfFood;
-    cin >> amountOfPortions;
+// Function that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
+vector<int> dijkstra(vector<vector<int>> graph, int src, int amountOfVertices)
+{
+    vector<int> dist(amountOfVertices); // The output array.  dist[i] will hold the shortest
+    // distance from src to i
 
-    vector<vector<double>> foodsAtBuffet(amountOfFood);
-    for(int i = 0;i < amountOfFood; i++){
-        vector<double> food(2);
-        string nameOfFood; // does not matter
-        cin >> nameOfFood;
-        cin >> food[0];
-        cin >> food[1];
-        foodsAtBuffet[i] = food;
+    bool sptSet[amountOfVertices]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as false
+    for (int i = 0; i < amountOfVertices; i++) {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
     }
-    sort(foodsAtBuffet.begin(), foodsAtBuffet.end(),sortcol);
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
 
+    // Find shortest path for all vertices
+    for (int count = 0; count < amountOfVertices - 1; count++) {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        int u = minDistance(dist, sptSet);
 
-    double score = 0;
-    int portionsAlreadyTaken = 0;
-    int currentIndex = 0;
-    while(portionsAlreadyTaken < amountOfPortions){
-        if(foodsAtBuffet[currentIndex][0] <= 0){
-            currentIndex++;
-        }
-        score += foodsAtBuffet[currentIndex][1];
-        foodsAtBuffet[currentIndex][0]--;
-        portionsAlreadyTaken++;
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < amountOfVertices; v++)
+
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+                && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
     }
 
-    cout << score << endl;
+    // print the constructed distance array
+    return dist;
+}
+
+vector<vector<int>> createAdjacencyMatrix(){
+
+}
+
+// driver program to test above function
+int main()
+{
+    /* Let us create the example graph discussed above */
+    vector<vector<int>> graph = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+    vector<int> distanceToVertices = dijkstra(graph, 0,graph[0].size());
+    cout << distanceToVertices[1];
     return 0;
 }
